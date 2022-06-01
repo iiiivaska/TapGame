7d4439da40fb0b7fbef27d18643ddb1878b25b76
iiiivasaka <iiiivsaka@yandex.ru>
2022-06-01 19:07:16 +0300

    Main view added, way to set UIView borderы

---
+++ TapGame/README.md
@@ -0,0 +1,14 @@
+#  TapGame
+[«Разработка под iOS. Начинаем»](https://www.youtube.com/watch?v=RR5o5ki0888&list=PLQC2_0cDcSKA0zy20X9c5rQKNg3rkSK7c&index=25)
+
+
+## Установить границы UIView
+```swift 
+gameView.layer.borderWidth = 1 
+gameView.layer.borderColor = UIColor.gray.cgColor 
+gameView.layer.cornerRadius = 5 
+```
+<p align="center">
+  <img width=300 src="MDFiles/gameViewBorder.png">
+</p>
+
--- TapGame/ViewController.swift
+++ TapGame/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var gameView: UIView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        gameView.layer.borderWidth = 1
+        gameView.layer.borderColor = UIColor.gray.cgColor
+        gameView.layer.cornerRadius = 5
     }
 
-
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        timeLabel.text = "Время: \(sender.value) сек"
+    }
 }
 
